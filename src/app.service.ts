cbdb97cd7d6415d7414840ecc867ff8b0247fcb6
Tomás Elizondo <tomaslelizondo@gmail.com>
2022-04-15 01:39:06 -0300

    config type

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from 'config';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string, // was replaced for the config service
     @Inject('TASKS') private tasks: any, // this is injected from the global gatabase module
-    private configService: ConfigService,
+    // private configService: ConfigService, // was replaced for TypeConfig
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
-    const apiKey = this.configService.get('API_KEY'); // the better way
+    // const apiKey = this.configService.get('API_KEY'); // the better way
+    const apiKey = this.configService.apiKey;
     console.log(apiKey);
     return `Hello World! ${apiKey}
     Tasks:
